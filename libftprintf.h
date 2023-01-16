756e3ab17cbdea10c08b8c22e3bc5dac26f90306
Pc fixe anto <anto73grillet@gmail.com>
2023-01-17 00:45:29 +0100

    debut d'un truc

---
+++ libftprintf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrillet <anto73grillet@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 17:29:05 by agrillet          #+#    #+#             */
+/*   Updated: 2023/01/16 17:29:05 by agrillet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+#include "libft.h"
+#include <stdarg.h>
+
+int ft_printf(const char *, ...);
